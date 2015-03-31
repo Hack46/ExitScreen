d291562ea08f45cee1a194f4abaf1ac3dec516c1
angieskazka <konzeptmeister@gmail.com>
2015-03-31 12:13:48 +0200

    Many styling changes

--- client/resize.js
+++ client/resize.js
@@ -3,12 +3,13 @@
     
     function resize(){
         DEPARTURES_AXIS_HEIGHT = window.innerHeight*0.05;//px
-        WEATHER_AXIS_HEIGHT = 20;//px
+        WEATHER_AXIS_HEIGHT = window.innerHeight*0.05;//px
         DEPARTURES_WIDTH_GRAPH = 1; //fraction
         WIDTH = window.innerWidth;
         WEATHER_HEIGHT = parseInt(d3.select("#weather").style("height")) - WEATHER_AXIS_HEIGHT;
         DEPARTURES_ROW_HEIGHT = (parseInt(d3.select("#departures").style("height")) - DEPARTURES_AXIS_HEIGHT) / config.departures.length;
         WEATHER_WIDTH_ONE = WIDTH/config.weather.steps;
+        WEATHER_CURTAIN_HEIGHT = 8; //px
         
         TEXTHEIGHT_LAGOM = Math.min(24, Math.max(6, DEPARTURES_ROW_HEIGHT - 8));
         TEXTHEIGHT_SMALL = Math.min(18, Math.max(4, DEPARTURES_ROW_HEIGHT - 20));
@@ -74,34 +75,51 @@
                 
             })
         d3.select("#weather").select("svg").selectAll(".column")
-            .attr("transform",function(d,i){return "translate("+(i* WEATHER_WIDTH_ONE)+",0)"})
+            .attr("width",WEATHER_WIDTH_ONE)
+            .attr("height",WEATHER_HEIGHT+WEATHER_AXIS_HEIGHT)
+            .attr("x",function(d,i){return i* WEATHER_WIDTH_ONE})
             .each(function(departure,index){
                 var weatherOne = d3.select(this);
             
                 weatherOne.select("circle.sun")
+                    .attr("r", WEATHER_WIDTH_ONE/2*0.8)
                     .attr("cx", WEATHER_WIDTH_ONE/2);
+            
+                var curtainsData = new Array(Math.floor(WEATHER_WIDTH_ONE/WEATHER_CURTAIN_HEIGHT));
+                var curtains = weatherOne.select("g.curtains").selectAll("rect").data(curtainsData);
+                curtains.exit().remove();
+                curtains.enter().append("rect");
+                curtains.attr("width", WEATHER_WIDTH_ONE);
+            
                 weatherOne.select("rect.rain")
                     .attr("width", WEATHER_WIDTH_ONE/2)
                     .attr("x", WEATHER_WIDTH_ONE/4);
                 weatherOne.select("rect.wind")
-                    .attr("x", WEATHER_WIDTH_ONE/2-WEATHER_WIDTH_ONE/6/2)
-                    .attr("width", WEATHER_WIDTH_ONE/6);
+                    .attr("x", WEATHER_WIDTH_ONE-WEATHER_WIDTH_ONE/8)
+                    .attr("width", WEATHER_WIDTH_ONE/8);
                         
-                weatherOne.select("text.sun")
-                    .attr("x", WEATHER_WIDTH_ONE/2);
                 weatherOne.select("text.rain")
                     .attr("x", WEATHER_WIDTH_ONE/2);
-                weatherOne.select("text.wind")
-                    .attr("x", WEATHER_WIDTH_ONE/2)
-                    .attr("dy", "-0.32em");
-                weatherOne.select("text.temp")
+                weatherOne.selectAll("text.wind")
+                    .attr("x", WEATHER_WIDTH_ONE)
+                    .attr("dy", "-0.18em")
+                    .attr("dx", "-0.72em");
+                weatherOne.selectAll("text.temp")
                     .attr("x", WEATHER_WIDTH_ONE/2)
                     .attr("dy", "0.32em");
             
             
             
-                weatherOne.select("line.grid")
-                        .attr("y2",WEATHER_HEIGHT)                
+                weatherOne.select("rect.grid")
+                        .attr("y",0)                
+                        .attr("x",WEATHER_WIDTH_ONE-1)                
+                        .attr("height",WEATHER_HEIGHT)                
+                        .attr("width",1)  
+                weatherOne.select("rect.grid.bottom")
+                        .attr("y",WEATHER_HEIGHT)                
+                        .attr("x",0)                
+                        .attr("height",1)                
+                        .attr("width",WEATHER_WIDTH_ONE)  
                 weatherOne.select("text.grid")
                         .attr("x", WEATHER_WIDTH_ONE/2)
                         .attr("y",WEATHER_HEIGHT + WEATHER_AXIS_HEIGHT);
